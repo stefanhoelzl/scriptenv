bf558a4c0583314d79941c5e3c901d59f0dbbde7
Stefan Hoelzl <1478183+stefanhoelzl@users.noreply.github.com>
2021-07-16 17:30:47 +0000

    [tooling] configure pytest in code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring
+"""configures pytest"""
 import sys
 from pathlib import Path
 from typing import Generator
@@ -6,6 +6,36 @@ from unittest.mock import patch
 
 import appdirs
 import pytest
+from _pytest.config import Config
+from pytest_cov.plugin import CovPlugin
+
+
+@pytest.mark.tryfirst
+def pytest_configure(config: Config) -> None:
+    """Setup default pytest options."""
+    config.option.newfirst = True
+    config.option.failedfirst = True
+    config.option.capture = "no"
+    config.option.tbstyle = "short"
+
+    config.option.pylint = True
+    config.option.black = True
+    config.option.isort = True
+
+    config.option.mypy = True
+    config.option.mypy_ignore_missing_imports = True
+    config.pluginmanager.getplugin("mypy").mypy_argv.extend(["--strict"])
+
+    config.option.mccabe = True
+    config.addinivalue_line("mccabe-complexity", "5")
+
+    config.option.cov_source = ["scriptenv"]
+    config.option.cov_fail_under = 100
+    config.option.cov_report = "term-missing"
+    config.option.cov_branch = True
+    config.pluginmanager.register(
+        CovPlugin(config.option, config.pluginmanager), "_cov"
+    )
 
 
 @pytest.fixture(autouse=True, scope="function")
